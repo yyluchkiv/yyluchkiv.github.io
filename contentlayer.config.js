33f583ac78b54a0ae1f575b60035ce2a06d19da3
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2023-10-26 10:49:28 +0300

    migration #1

---
+++ contentlayer.config.js
@@ -0,0 +1,73 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import rehypePrettyCode from 'rehype-pretty-code'
+
+/** @type {import('contentlayer/source-files').ComputedFields} */
+const computedFields = {
+  slug: {
+    type: 'string',
+    resolve: (doc) => doc.url,
+  },
+  structuredData: {
+    type: 'object',
+    resolve: (doc) => ({
+      headline: doc.title,
+      datePublished: doc.publishedAt,
+      dateModified: doc.publishedAt,
+      description: doc.summary,
+      author: {
+        '@type': 'Person',
+        name: 'Yurii Luchkiv'
+      },
+    }),
+  },
+};
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: {
+      type: 'string',
+      required: true
+    },
+    publishedAt: {
+      type: 'string',
+      required: true
+    },
+    summary: {
+      type: 'string',
+      required: true
+    },
+    image: {
+      type: 'string'
+    },
+    category: {
+      type: 'string',
+      required: true
+    },
+    url: {
+      type: 'string'
+    },
+    hide: {
+      type: 'boolean'
+    }
+  },
+  computedFields
+}))
+
+export default makeSource({
+  contentDirPath: 'src/content',
+  documentTypes: [Post],
+  mdx: {
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: 'one-dark-pro',
+          grid: false
+        }
+      ]
+    ]
+  }
+})
--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: []
+  }
+})
+const { withContentlayer } = require('next-contentlayer')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  reactStrictMode: true,
+  swcMinify: true,
+  transpilePackages: ['react-tweet']
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
+module.exports = withContentlayer(nextConfig)
