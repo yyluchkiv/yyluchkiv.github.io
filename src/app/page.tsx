8db5cd37d1fb827c720e62e2eb1346bb9e51f62a
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2024-02-18 22:28:46 +0200

    v1.2.0 - new "Home page" (technology stack section)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,8 +50,8 @@ export default function Home() {
       <div className={ Styles.home }>
         <p>
           I am a software engineer (since 2011) and a father (since 2016). As long as I can remember, I have always been curious about how things work.
-          I currently work at <IconLink iconSource={ Tech1SmallIcon } alt='Tech1' text='Tech1' link='https://tech1.io/' />,
-          where I build apps and deliver ideas to the market using <IconLink iconSource={ JavaIcon } alt='Java' text='Java' link='https://dev.java/' />&nbsp;
+          I currently work at <IconLink iconSource={ Tech1SmallIcon } alt='Tech1' text='Tech1' link='https://tech1.io/' />.
+          I build apps and deliver ideas to the market using <IconLink iconSource={ JavaIcon } alt='Java' text='Java' link='https://dev.java/' />&nbsp;
           <IconLink iconSource={ SpringIcon } alt='Spring' text='Spring' link='https://spring.io/projects/spring-framework' />&nbsp;
           <IconLink iconSource={ NextIcon } alt='Next.js' text='Next.js' link='https://nextjs.org/' />&nbsp;
           <IconLink iconSource={ ReactIcon } alt='React' text='React' link='https://react.dev/' />&nbsp;
@@ -64,14 +64,14 @@ export default function Home() {
           When I was young I wasn&apos;t sure I should become a software engineer.
           However, I started my professional education when I was 12 years old in Lviv Physics and
           Mathematics Lyceum and completed it at the age of 21 in the Applied Mathematics and Informatics department at Lviv Ivan Franko National University.
-          I had some viable options in non software development area (or at least I thought they were viable at that time).
+          I had some viable options in the non-software development area (or at least I thought they were viable at that time).
           But then I tried to start a professional career as a software engineer. It worked, and I liked it.
         </p>
         <p className={ Styles.paragraph }>
-          Right now I am a Java-ecosystem software engineer.
+          Right now I am a mostly Java software engineer.
           In the early days of my career, I wanted to be a full-stack developer to be able to complete apps individually.
           Or at least in the early stages.
-          Later on I understood that to be a really good full-stack developer you need to be a backend developer during your work time and be a frontend developer in your spare time.
+          Later on, I understood that to be a really good full-stack developer you need to be a backend developer during your work time and be a frontend developer in your spare time.
         </p>
         <p className={ Styles.paragraph }>
           Overall, I like working closely with decision-makers, teaching and mentoring young people in our industry.
@@ -88,7 +88,15 @@ export default function Home() {
           <Badge iconSource={ ConscensiaIcon } alt='Conscensia' link='https://conscensia.com/' />
           <Badge iconSource={ SystematicIcon } alt='Systematic' link='https://systematic.com/en-gb/industries/defence/products/sitaware-suite/sitaware-headquarters' />
         </div>
-
+        <p className={ Styles.paragraph }>
+            Technologies that I am experienced in:
+            <ul className={ Styles.technologies }>
+                <li className={ Styles.technology }>— java, spring boot, spring data, apache maven</li>
+                <li className={ Styles.technology }>— ubuntu, docker, docker-compose, github actions, jenkins</li>
+                <li className={ Styles.technology }>— postgres, mongodb</li>
+                <li className={ Styles.technology }>— javascript, vue.js</li>
+            </ul>
+        </p>
 
         {/*Google Analytics*/}
         <div className="container">
