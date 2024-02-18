d9434de99f63cfb3791c3612cce229af9b69d4e7
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2024-02-18 22:10:50 +0200

    v1.2.0 - new "Home page" (no technology stack section)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,39 +49,37 @@ export default function Home() {
   return (
       <div className={ Styles.home }>
         <p>
-          I am a software engineer, a father and always curious about how things works.
+          I am a software engineer (since 2011) and a father (since 2016). As long as I can remember, I have always been curious about how things work.
           I currently work at <IconLink iconSource={ Tech1SmallIcon } alt='Tech1' text='Tech1' link='https://tech1.io/' />,
-          where I build and grow efficient teams to deliver ideas to the market
-          using <IconLink iconSource={ JavaIcon } alt='Java' text='Java' link='https://dev.java/' />&nbsp;
+          where I build apps and deliver ideas to the market using <IconLink iconSource={ JavaIcon } alt='Java' text='Java' link='https://dev.java/' />&nbsp;
           <IconLink iconSource={ SpringIcon } alt='Spring' text='Spring' link='https://spring.io/projects/spring-framework' />&nbsp;
           <IconLink iconSource={ NextIcon } alt='Next.js' text='Next.js' link='https://nextjs.org/' />&nbsp;
           <IconLink iconSource={ ReactIcon } alt='React' text='React' link='https://react.dev/' />&nbsp;
           <IconLink iconSource={ VueIcon } alt='Vue.js' text='Vue.js' link='https://vuejs.org/' /> and <IconLink iconSource={ AwsIcon } alt='AWS' text='AWS' link='https://aws.amazon.com/' />.
         </p>
-        <p>
-          I have been working in the software development area since 2011.
-          When I was young I wasn&apos;t sure I should become a software engineer.
-          However, I started my professional education at the age of twelve in Lviv Physics and
-          Mathematics Lyceum and completed it at the age of twenty-one in the Applied Mathematics and Informatics department at Lviv Ivan Franko National University.
-          I had some viable options or at least I thought they were viable at that time — I was only 19 years old.
-          Silly me. Then I tried to start a professional career as a software engineer. It worked, and I liked it.
-        </p>
         <div className={ Styles.gallery }>
           <ImageGallery gallery={ gallery01 } gap={ 15 } imageBorderRadius={ 2 } orientation={ 'h' } preview={ false } />
         </div>
-        <p>
-          Right now I want to believe I am a Java-ecosystem software engineer. In the early days of my career,
-          I wanted to be a full-stack developer to be able to complete apps individually.
-          Or at least in the early stages. But the time came and I understood that to be
-          a really good full-stack developer you need to be a backend developer during your work time and be a frontend developer in your spare time.
+        <p className={ Styles.paragraph }>
+          When I was young I wasn&apos;t sure I should become a software engineer.
+          However, I started my professional education when I was 12 years old in Lviv Physics and
+          Mathematics Lyceum and completed it at the age of 21 in the Applied Mathematics and Informatics department at Lviv Ivan Franko National University.
+          I had some viable options in non software development area (or at least I thought they were viable at that time).
+          But then I tried to start a professional career as a software engineer. It worked, and I liked it.
         </p>
-        <p>
+        <p className={ Styles.paragraph }>
+          Right now I am a Java-ecosystem software engineer.
+          In the early days of my career, I wanted to be a full-stack developer to be able to complete apps individually.
+          Or at least in the early stages.
+          Later on I understood that to be a really good full-stack developer you need to be a backend developer during your work time and be a frontend developer in your spare time.
+        </p>
+        <p className={ Styles.paragraph }>
           Overall, I like working closely with decision-makers, teaching and mentoring young people in our industry.
         </p>
         <div className={ Styles.gallery }>
           <ImageGallery gallery={ gallery02 } gap={ 15 } imageBorderRadius={ 2 } orientation='h' preview={ false } />
         </div>
-        <p>
+        <p className={ Styles.paragraph }>
           Over the past decade I worked with multiple companies, projects and teams.
         </p>
         <div className={ Styles.badges }>
