7c68735b36e579978734744b5248cffd028cb9ca
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2024-08-22 11:49:41 +0300

    v130 #2 - clean footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,22 +6,8 @@ import Image, { StaticImageData } from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
 import DateUtility from '@/src/utilities/DateUtility'
-import BehanceMonoIcon from '@/public/assets/svgs/icons/Footer/Mono/BehanceMono.svg'
-import BehanceColorIcon from '@/public/assets/svgs/icons/Footer/Color/BehanceColor.svg'
-import ClutchMonoIcon from '@/public/assets/svgs/icons/Footer/Mono/ClutchMono.svg'
-import ClutchColorIcon from '@/public/assets/svgs/icons/Footer/Color/ClutchColor.svg'
-import DouMonoIcon from '@/public/assets/svgs/icons/Footer/Mono/DouMono.svg'
-import DouColorIcon from '@/public/assets/svgs/icons/Footer/Color/DouColor.svg'
-import FacebookMonoIcon from '@/public/assets/svgs/icons/Footer/Mono/FacebookMono.svg'
-import FacebookColorIcon from '@/public/assets/svgs/icons/Footer/Color/FacebookColor.svg'
 import GithubMonoIcon from '@/public/assets/svgs/icons/Footer/Mono/GithubMono.svg'
 import GithubColorIcon from '@/public/assets/svgs/icons/Footer/Color/GithubColor.svg'
-import GmailMonoIcon from '@/public/assets/svgs/icons/Footer/Mono/GmailMono.svg'
-import GmailColorIcon from '@/public/assets/svgs/icons/Footer/Color/GmailColor.svg'
-import InstagramMonoIcon from '@/public/assets/svgs/icons/Footer/Mono/InstagramMono.svg'
-import InstagramColorIcon from '@/public/assets/svgs/icons/Footer/Color/InstagramColor.svg'
-import LinkedinMonoIcon from '@/public/assets/svgs/icons/Footer/Mono/LinkedinMono.svg'
-import LinkedinColorIcon from '@/public/assets/svgs/icons/Footer/Color/LinkedinColor.svg'
 import TwitterMonoIcon from '@/public/assets/svgs/icons/Footer/Mono/TwitterMono.svg'
 import TwitterColorIcon from '@/public/assets/svgs/icons/Footer/Color/TwitterColor.svg'
 import YoutubeMonoIcon from '@/public/assets/svgs/icons/Footer/Mono/YoutubeMono.svg'
@@ -51,115 +37,42 @@ const SocialLink: React.FC<{ socialItem: SocialItem }> = ({ socialItem }) => {
   return (
       <li className={ Styles.socialItem } key={ socialItem.alt } onMouseEnter={ onMouseEnterLink } onMouseLeave={ onMouseLeaveLink } onClick={ onMouseLeaveLink }>
         <Link className={ Styles.socialLink } href={ socialItem.href } target='_blank'>
-          <Image className={ `${ Styles.icon } ${ isHovering && Styles.iconHidden }` } width={ 22 } height={ 22 } src={ socialItem.icon } alt={ socialItem.alt } priority={ true } />
-          <Image className={ `${ Styles.hoverIcon } ${ isHovering && Styles.hoverIconVisible }` } width={ 22 } height={ 22 } src={ socialItem.hoverIcon } alt={ socialItem.alt } />
+          <Image className={ `${ Styles.icon } ${ isHovering && Styles.iconHidden }` } width={ 25 } height={ 25 } src={ socialItem.icon } alt={ socialItem.alt } priority={ true } />
+          <Image className={ `${ Styles.hoverIcon } ${ isHovering && Styles.hoverIconVisible }` } width={ 25 } height={ 25 } src={ socialItem.hoverIcon } alt={ socialItem.alt } />
         </Link>
       </li>
   )
 }
 
-const Footer: React.FC<{
-  gmailVisible?: boolean,
-  githubVisible?: boolean,
-  upworkVisible?: boolean,
-  twitterVisible?: boolean,
-  linkedinVisible?: boolean,
-  instagramVisible?: boolean,
-  facebookVisible?: boolean,
-  youtubeVisible?: boolean,
-  douVisible?: boolean,
-  behanceVisible?: boolean,
-  clutchVisible?: boolean
-}> = ({
-  gmailVisible = true,
-  githubVisible = true,
-  upworkVisible = true,
-  twitterVisible = true,
-  linkedinVisible = true,
-  instagramVisible = true,
-  facebookVisible = true,
-  youtubeVisible = false,
-  douVisible = false,
-  behanceVisible = false,
-  clutchVisible = false
-}) => {
+const Footer: React.FC = () => {
   const socials: SocialItem[] = [
     {
       alt: 'Github',
-      visible: githubVisible,
+      visible: true,
       icon: GithubMonoIcon,
       hoverIcon: GithubColorIcon,
       href: 'https://github.com/yyluchkiv'
     },
     {
-      alt: 'Gmail',
-      visible: gmailVisible,
-      icon: GmailMonoIcon,
-      hoverIcon: GmailColorIcon,
-      href: 'mailto:yyluchkiv@gmail.com'
-    },
-    {
       alt: 'Upwork',
-      visible: upworkVisible,
+      visible: true,
       icon: UpworkMonoIcon,
       hoverIcon: UpworkColorIcon,
       href: 'https://www.upwork.com/freelancers/yyluchkiv'
     },
     {
       alt: 'Twitter',
-      visible: twitterVisible,
+      visible: true,
       icon: TwitterMonoIcon,
       hoverIcon: TwitterColorIcon,
       href: 'https://twitter.com/yyluchkiv'
     },
     {
-      alt: 'Linkedin',
-      visible: linkedinVisible,
-      icon: LinkedinMonoIcon,
-      hoverIcon: LinkedinColorIcon,
-      href: 'https://www.linkedin.com/in/yyluchkiv'
-    },
-    {
-      alt: 'Facebook',
-      visible: facebookVisible,
-      icon: FacebookMonoIcon,
-      hoverIcon: FacebookColorIcon,
-      href: 'https://www.facebook.com/yyluchkiv'
-    },
-    {
-      alt: 'Instagram',
-      visible: instagramVisible,
-      icon: InstagramMonoIcon,
-      hoverIcon: InstagramColorIcon,
-      href: 'https://www.instagram.com/yyluchkiv'
-    },
-    {
       alt: 'Youtube',
-      visible: youtubeVisible,
+      visible: false,
       icon: YoutubeMonoIcon,
       hoverIcon: YoutubeColorIcon,
       href: 'https://www.youtube.com/'
-    },
-    {
-      alt: 'DOU',
-      visible: douVisible,
-      icon: DouMonoIcon,
-      hoverIcon: DouColorIcon,
-      href: 'https://dou.ua/'
-    },
-    {
-      alt: 'Behance',
-      visible: behanceVisible,
-      icon: BehanceMonoIcon,
-      hoverIcon: BehanceColorIcon,
-      href: 'https://www.behance.net/'
-    },
-    {
-      alt: 'Clutch',
-      visible: clutchVisible,
-      icon: ClutchMonoIcon,
-      hoverIcon: ClutchColorIcon,
-      href: 'https://clutch.co/'
     }
   ]
 
