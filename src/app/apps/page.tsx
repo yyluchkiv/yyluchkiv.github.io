378fdecfba8a95387ae357ab1c0427879f1f57b8
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2024-08-23 21:41:52 +0300

    v130 #13 - completed

--- src/app/apps/page.tsx
+++ src/app/apps/page.tsx
@@ -3,9 +3,9 @@ import ArrowLink from "@/src/components/Links/ArrowLink/ArrowLink";
 export default function About () {
   return (
     <div className="any-page">
-      <h2>
+      <p>
         <ArrowLink link={"https://dadapp.app/"} text={"dadapp.app"} imageVisible={true}/>
-      </h2>
+      </p>
       <p className="any-page-paragraph">
         Family data in your pocket. No more spreadsheets, no more notes, no more &quot;One second, I&apos;ll call my
         wife to check this&quot;.
@@ -17,9 +17,11 @@ export default function About () {
         <ArrowLink link={"https://dadapp.app/"} text={"Download on Google Play"} imageVisible={false}/>
       </p>
       <br/>
-      <h2 className="any-page-paragraph">
+      <hr/>
+      <br/>
+      <p className="any-page-paragraph">
         <ArrowLink link={"https://chromewebstore.google.com/detail/password-generator/nbnjbddbbcnnlceoglkfoolmknejieoi?hl=en"} text={"password generator"} imageVisible={true}/>
-      </h2>
+      </p>
       <p className="any-page-paragraph">
         Chrome Extension: Password Generator - simple, secure, one-click password generator
       </p>
