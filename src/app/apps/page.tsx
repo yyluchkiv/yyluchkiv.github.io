3c0d28aefac4f97aca8b97d2417adaa0dcaec401
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2024-08-23 21:22:55 +0300

    v130 #11

--- src/app/apps/page.tsx
+++ src/app/apps/page.tsx
@@ -1,7 +1,14 @@
+import ArrowLink from "@/src/components/Links/ArrowLink/ArrowLink";
+
 export default function About () {
   return (
     <div className="any-page">
-      Apps
+      <p className="any-page-paragraph">
+        <ArrowLink link={"https://dadapp.app/"} text={"dadapp.app"}/>
+      </p>
+      <p className="any-page-paragraph">
+        Family data in your pocket. No more spreadsheets, no more notes, no more &quot;One second, I&apos;ll call my wife to check this&quot;.
+      </p>
     </div>
   )
 }
